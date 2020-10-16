b5d07446be8b56037a73f34ee5a03b8f18bdb235
ind-srabonti <bonti83@gmail.com>
2020-10-16 05:04:00 -0400

    Updates

---
+++ precache-manifest.0ef41da8ecca7d66765e8d2810c418ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4449fb72b0d625cb7d65899f4ec90d5f",
+    "url": "/frontendDoggo/doggoportal/index.html"
+  },
+  {
+    "revision": "b4e0b1d561227482a74a",
+    "url": "/frontendDoggo/doggoportal/static/css/2.cd1dd82c.chunk.css"
+  },
+  {
+    "revision": "db82418df1e9dd98323a",
+    "url": "/frontendDoggo/doggoportal/static/css/main.5e6e88a8.chunk.css"
+  },
+  {
+    "revision": "b4e0b1d561227482a74a",
+    "url": "/frontendDoggo/doggoportal/static/js/2.ea08a60c.chunk.js"
+  },
+  {
+    "revision": "f576af3254df0679363d3e6ffe804e36",
+    "url": "/frontendDoggo/doggoportal/static/js/2.ea08a60c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db82418df1e9dd98323a",
+    "url": "/frontendDoggo/doggoportal/static/js/main.55191c5f.chunk.js"
+  },
+  {
+    "revision": "6a92579ebc1bcb5b4b58",
+    "url": "/frontendDoggo/doggoportal/static/js/runtime-main.8c8b9fb7.js"
+  }
+]);
